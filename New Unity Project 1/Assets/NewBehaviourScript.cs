a138464dc27b9d9625c166ef8df9dbc707e83d3a
Chaos-Lord <dd4791.mariokarter@hotmail.co.uk>
2014-02-17 12:06:45 +0000

    pedestians implemented in the area around the player

---
+++ New Unity Project 1/Assets/NewBehaviourScript.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+
+
+
+
+public class NewBehaviourScript : MonoBehaviour {//put on all pedestrians, makes them move and sets thier targets
+	private NavMeshAgent agent;//always attach a navmeshagent too
+
+	public int target;
+	int last;
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Vector3.Distance(transform.position, nodes.nodeList [target].loc) < 2) {//if at node
+			if(Vector3.Distance(PlayerStore.PlayerLoc , nodes.nodeList[target].loc) > 100)//if out of range
+			{
+				Destroy(this.gameObject);
+			
+			}
+			else{
+				NavNode.RNG = Random.Range(0,nodes.nodeList[target].adjNodes.Length);//choose adacent
+			if (nodes.nodeList[target].adjNodes[NavNode.RNG] != last)
+			{
+				last = target;//go to adjacent
+				target = nodes.nodeList[target].adjNodes[NavNode.RNG];
+				agent.SetDestination(nodes.nodeList[target].loc);
+			}
+						
+			}
+				}
+	
+	}
+}
