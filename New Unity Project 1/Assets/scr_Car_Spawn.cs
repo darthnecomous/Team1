743844338d51e44b0c9e8511e3be805656ff06df
Jackthehobbit <scunnyrule45@hotmail.co.uk>
2014-02-20 16:37:45 +0000

    Sprint 1 Final

---
+++ New Unity Project 1/Assets/scr_Car_Spawn.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scr_Car_Spawn : MonoBehaviour {
+	public int R;
+	GameObject Car;
+	GameObject House;
+    Quaternion HouseFacing;
+	int HouseP;
+	// Use this for initialization
+	void Start () {
+		Car = GameObject.Find ("Car_Prefab");
+		R = Random.Range (0, 8);
+		HouseP = Random.Range (0, 4);
+		HouseFacing = transform.rotation;
+
+		if (R == 1) {
+			Instantiate(Car);
+			//Car.transform.position = transform.localPosition + new Vector3 (0,0,11);
+			Car.transform.position = transform.localPosition - (transform.right* 13);
+			//Car.transform.position = transform.localPosition - (transform.forward* HouseP);
+
+			Car.transform.rotation = HouseFacing;
+			Car.transform.Rotate(Car.transform.up,92f);
+			//Car.transform.position = transform.position + (transform.forward * 50);
+		}
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+	}
+}
